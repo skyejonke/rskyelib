e7d35ff79d068e84f66cb2515fc76e9fe9e580d1
Skye J <skye@skyenet.online>
2021-10-21 11:51:31 -0400

    Added simple log function

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+use structopt::StructOpt;
+use chrono::{Local};
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short, long)]
+    verbose :bool,
+}
+// This will print a log, with the current time, of whatever string you give it. This is only true if it uses the -v or --verbose argument.
+pub fn log (inpt :String) {
+    let opt = Opt::from_args();
+    if opt.verbose {
+        let time = Local::now();
+        println!("{}:\t{}", time.format("%X"), inpt);
+    }
+}
+
