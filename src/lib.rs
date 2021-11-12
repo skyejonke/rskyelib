0065b4333ff0bbbb834192709bfe6a2bea85f0af
Skye Jonke <skye@skyenet.online>
2021-11-12 11:07:23 -0500

    Structopt didn't work well, made it actually work, lol

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-use structopt::StructOpt;
 use chrono::{Local};
+use std::env;
 // #[cfg(test)]
 // mod tests {
 //     #[test]
@@ -9,40 +9,18 @@ use chrono::{Local};
 // }
 
 
-#[derive(Debug, StructOpt)] #[structopt(name = "Cleanup", about = "Cleans up a directory.")]
-struct Opt {
-    /// Print lots of information?
-    #[structopt(short, long)]
-    verbose: bool,
-
-    /// Input Path
-    #[structopt(short = "i", long = "input", default_value = "/home/skye/Downloads/")]
-    inpt_path: String,
-
-    /// Output Path
-    #[structopt(
-        short = "o",
-        long = "output",
-        default_value = "/home/skye/Documents/DownloadArchives/"
-    )]
-    out_path: String,
-
-    /// Timeframe (in days)
-    #[structopt(short = "t", long = "timeframe", default_value = "7")]
-    time_frame: u32,
-}
 // This will print a log, with the current time, of whatever string you give it. This is only true if it uses the -v or --verbose argument.
 pub fn log (inpt :&str) {
-    let opt = Opt::from_args();
-    if opt.verbose {
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&"-v".to_string()) || args.contains(&"--verbose".to_string()) {
         let time = Local::now();
         println!("{}:\t{}", time.format("%X"), inpt);
     }
 }
 
 pub fn elog (inpt :&str) {
-    let opt = Opt::from_args();
-    if opt.verbose {
+    let args: Vec<String> = env::args().collect();
+    if args.contains(&"-v".to_string()) || args.contains(&"--verbose".to_string()) {
         let time = Local::now();
         eprintln!("{}:\t{}", time.format("%X"), inpt);
     }
